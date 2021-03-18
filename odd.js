63dd488d9898f1faf320621669ea13074a015ac9
Mona Waseem <monaqwaseem@gmail.com>
2021-03-18 16:16:06 +0000

    Odd Checker Challenge

---
+++ odd.js
@@ -0,0 +1,10 @@
+//Define a function called isOdd.
+//This function should take a number as a parameter and return true if it is odd and false if not.
+
+const isOdd = function (num) {
+  return num % 2 !== 0;
+
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+console.log(`11 is odd: ${isOdd(11)}`);
