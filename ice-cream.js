f147cbd992750a2bfc092c88849d7910f78895bc
Mona Waseem <monaqwaseem@gmail.com>
2021-03-20 02:57:46 +0000

    Baking Banana Bread with iterations and arrays.

--- ice-cream.js
+++ ice-cream.js
@@ -1,10 +1,10 @@
-// Do not modify these first two lines
-const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
-// console.log(iceCreamFlavours);
+// // Do not modify these first two lines
+// const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+// // console.log(iceCreamFlavours);
 
-// // Your code below here...
-// iceCreamFlavours[5] = 'root beer'
-// console.log(iceCreamFlavours);
+// // // Your code below here...
+// // iceCreamFlavours[5] = 'root beer'
+// // console.log(iceCreamFlavours);
 
-iceCreamFlavours[0];
-console.log(iceCreamFlavours);
+// iceCreamFlavours[0];
+// console.log(iceCreamFlavours);
---
+++ ingredients.js
@@ -0,0 +1,18 @@
+// Iterating over banana bread "mmmmmmm"
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+//While loop ingredients
+// let i = 0
+// while (i < ingredients.length) {
+//   console.log(ingredients[i]);
+//   i++;
+// }
+
+//For loop ingredients
+for (i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+//Looping ingredients backwards
+for (let i = ingredients.length - 1; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
